1f1e8346283e3032dd735d884ea5bc2a98abd2b0
fatimazhumash <fatimazhumash872@gmail.com>
2025-08-06 11:36:03 +0500

    first commit

--- face-api.js
+++ face-api.js
@@ -1,3 +1,3 @@
-await faceapi.nets.tinyFaceDetector.loadFromUri('/models/tiny_face_detector_model');
-await faceapi.nets.faceLandmark68Net.loadFromUri('/models/face_landmark_68_model');
-await faceapi.nets.faceRecognitionNet.loadFromUri('/models/face_recognition_model');
+await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
